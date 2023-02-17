760238e488e00ba430f51562573ef9555a1b8530
johnnyyork13 <johnnyyork13@hotmail.com>
2023-02-16 20:21:03 -0500

    add html and CSS

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const tiles = document.getElementsByClassName('tile');
+const pOne = document.getElementById('pOne');
+const pTwo = document.getElementById('pTwo');
+const reset = document.getElementById('reset');
+
